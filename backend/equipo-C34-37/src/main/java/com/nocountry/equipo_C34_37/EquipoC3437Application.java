177055f05363a55de938d9cb55f8a57eb3774b05
VictorRoe <victorroe819@gmail.com>
2025-02-18 13:09:38 -0500

    Add Spring boot project

---
+++ backend/equipo-C34-37/src/main/java/com/nocountry/equipo_C34_37/EquipoC3437Application.java
@@ -0,0 +1,13 @@
+package com.nocountry.equipo_C34_37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoC3437Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipoC3437Application.class, args);
+	}
+
+}
