c53f09dcc28e114a4c30d4547e613df4e5c0f850
Esteban <estebabmur@gmail.com>
2025-02-27 21:05:19 -0500

    Remove secret from env required variables

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const ServerEnvSchema = z.object({
     JWT_EXPIRE_TIME: z.coerce.number(),
     COOKIE_NAME: z.string(),
-    SECRET: z.string(),
 });
 
 const ClientEnvSchema = z.object({
@@ -17,7 +16,6 @@ function createServerEnv() {
     const serverEnv = {
         JWT_EXPIRE_TIME: process.env.JWT_EXPIRE_TIME,
         COOKIE_NAME: process.env.COOKIE_NAME,
-        SECRET: process.env.SECRET,
     };
 
     const parsedEnv = ServerEnvSchema.safeParse(serverEnv);
