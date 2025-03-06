9b7495089b77bd6edb9bf4308453c2fc0c0cac52
Esteban <estebabmur@gmail.com>
2025-03-05 21:09:32 -0500

    Update import in

--- frontend/src/app/app/_components/app-sidebar.tsx
+++ frontend/src/app/app/_components/app-sidebar.tsx
@@ -6,12 +6,13 @@ import {
     SidebarMenu,
 } from "@/components/ui/sidebar";
 import SidebarMenuLinks from "@/app/app/_components/SidebarMenuLinks";
+import Image from "next/image";
 
 
 export default function AppSidebar() {
     return (
         <Sidebar variant="sidebar" collapsible="offcanvas">
-            <SidebarHeader className="mx-auto"><img src="/vercel.svg" alt="logo" className="size-12"/></SidebarHeader>
+            <SidebarHeader className="mx-auto"><Image src="/vercel.svg" alt="logo" className="size-12"/></SidebarHeader>
             <SidebarContent>
                 <SidebarMenu>
                     <SidebarMenuLinks />
